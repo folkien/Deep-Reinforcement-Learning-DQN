b3dd5196e564629bedeab072d5d5549fcdac8dba
Sławomir Paszko <folkus@gmail.com>
2024-01-04 12:23:27 +0100

    Fixes for cartpool training.

--- cartpole-q.py
+++ cartpole-q.py
@@ -19,38 +19,60 @@ state_bounds = list(zip(env.observation_space.low, env.observation_space.high))
 q_table_shape = n_states + [env.action_space.n]
 q_table = np.zeros(q_table_shape)
 
-# Parametry uczenia
+# Learning rate - used for updating Q-values.
+#  - Means how much we value new information compared to previous information.
 alpha = 0.1
-# gamma = 0.9
+# Discount factor - used for updating Q-values
+# - Means how much we value future rewards compared to present rewards.
 gamma = 0.99
 
-# Parametry eksploracji
+# Epsilon - used for exploration.
+# - Means how much we choose to explore instead of exploit.
 epsilon = 1.0
+# Epsilon decay - used for decreasing epsilon over time.
+# - Means how much we decay epsilon after each episode. (reduce exploration time)
 epsilon_decay = 0.995
 # Min epsilon
+# - Means minimum value % of exploration.
 min_epsilon = 0.01
 
 # Funkcja dyskretyzująca stan
-def discretize_state(state : tuple, state_bounds : list, n_states : list):
+def discretize_state(state : np.ndarray, 
+                     state_bounds : list[tuple], 
+                     n_states : list):
+    ''' Discretize a state vector to return a tuple of integers '''
     discretized = []
+
+    # State  : For each state variable
     for i in range(len(state)):
+        # State : Get the bounds for this variable
         scaling = (state[i] - state_bounds[i][0]) / (state_bounds[i][1] - state_bounds[i][0])
+
+        # State : Get the value for this variable
         new_state = int(round((n_states[i] - 1) * scaling))
         new_state = min(n_states[i] - 1, max(0, new_state))
+
+        # State : Add to the list of discretized values
         discretized.append(new_state)
 
     return tuple(discretized)
 
-# Funkcja wyboru akcji
-def choose_action(state):
+def choose_action(state : tuple) -> int:
+    ''' Choose an action for the current state according to the Q policy'''
+    # Exploration : If random number < epsilon
     if np.random.random() < epsilon:
         return env.action_space.sample() # Losowa akcja (eksploracja)
-    else:
-        return np.argmax(q_table[state]) # Najlepsza znana akcja (eksploatacja)
+    
+    # Q Policy : Best q value for this state
+    return np.argmax(q_table[state]) # Najlepsza znana akcja (eksploatacja)
 
 # Funkcja aktualizacji tabeli Q
-def update_q_table(state, action, reward, next_state):
+def update_q_table(state : tuple, action : int, reward : float, next_state : tuple):
+    ''' Update the Q table with the new Q value'''
+    # Q-Table : Get the best q value for the next state
     best_next_action = np.argmax(q_table[next_state])
+
+    # Q-Table : Compute the TD error (Bellman equation)
     q_table[state + (action,)] += alpha * (reward + gamma * q_table[next_state + (best_next_action,)] - q_table[state + (action,)])
 
 # Loop of training : For N episodes
@@ -64,10 +86,19 @@ for episode in range(1000):
     # Episode : Process episode until not done
     done = False
     while not done:
+        # Action : Choose action according to epsilon-greedy policy
         action = choose_action(state)
+
+        # Episode : Take action and get next state and reward
         next_state, reward, done, truncated,  _ = env.step(action)
+
+        # State : Discretize the next state to values according to n_states discretization
         next_state = discretize_state(next_state, state_bounds, n_states)
+
+        # Q-Table : Update Q-table
         update_q_table(state, action, reward, next_state)
+
+        # State : Update state
         state = next_state
 
     epsilon = max(epsilon * epsilon_decay, min_epsilon) # Zmniejszanie eksploracji

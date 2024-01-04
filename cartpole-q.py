32d8b2daa2eabe63e00ed09f1d4fbc981798dcd3
Sławomir Paszko <folkus@gmail.com>
2024-01-04 13:23:58 +0100

    Fixes.

--- cartpole-q.py
+++ cartpole-q.py
@@ -1,10 +1,19 @@
 from collections import namedtuple
+from enum import Enum
+from time import sleep, time
 import gym
 import numpy as np
 import math
 
 # CartPole-v1 state:
 CartPoleState = namedtuple('CartPoleState', ['cart_pos', 'cart_vel', 'pole_angle', 'pole_ang_vel'])
+
+# CartPole-v1 action: enum
+# - 0 - push cart to the left
+# - 1 - push cart to the right
+class CartPoleAction(int, Enum):
+    Left = 0
+    Right = 1
 
 # Inicjalizacja środowiska
 env = gym.make('CartPole-v1')
@@ -20,6 +29,7 @@ state_bounds = list(zip(env.observation_space.low, env.observation_space.high))
 # - Mamy 2 akcje (0 lub 1),
 # Pary (stan, akcja) są indeksowane w tabeli Q.
 # - czyli mamy 20*20*20*20*2=320000 pól w tabeli Q. WOW!
+# - Wartości w tabeli Q są inicjalizowane na 0 (nagród nie znamy).
 q_table_shape = n_states + [env.action_space.n]
 q_table = np.zeros(q_table_shape)
 
@@ -61,26 +71,35 @@ def discretize_state(state : np.ndarray,
 
     return CartPoleState(*discretized)
 
-def choose_action(state : tuple) -> int:
+def choose_action(state : tuple,
+                  force_optimal : bool = False
+                  ) -> CartPoleAction:
     ''' Choose an action for the current state according to the Q policy'''
-    # Exploration : If random number < epsilon
-    if np.random.random() < epsilon:
-        return env.action_space.sample() # Losowa akcja (eksploracja)
+    # Exploration : If random number < epsilon and not force optimal
+    if (np.random.random() < epsilon) and (not force_optimal):
+        return CartPoleAction(env.action_space.sample())
     
-    # Q Policy : Best q value for this state
-    return np.argmax(q_table[state]) # Najlepsza znana akcja (eksploatacja)
+    # Q Policy : Best q value for this state (argmax returns the index of the max value)
+    return CartPoleAction(np.argmax(q_table[state]))
 
 # Funkcja aktualizacji tabeli Q
-def update_q_table(state : tuple, action : int, reward : float, next_state : tuple):
+def update_q_table(state : tuple, action : CartPoleAction, reward : float, next_state : tuple):
     ''' Update the Q table with the new Q value'''
+    # Action : as int
+    action = int(action)
+
     # Q-Table : Get the best q value for the next state
     best_next_action = np.argmax(q_table[next_state])
+
 
     # Q-Table : Compute the TD error (Bellman equation)
     q_table[state + (action,)] += alpha * (reward + gamma * q_table[next_state + (best_next_action,)] - q_table[state + (action,)])
 
 # Loop of training : For N episodes
 for episode in range(1000):
+    # Info : Print episode informations
+    print(f"Episde {episode} started. Epsilon: {epsilon}")
+
     # State : Get initial episode state
     state_initial_dict = env.reset()
 
@@ -105,8 +124,59 @@ for episode in range(1000):
         # State : Update state
         state = next_state
 
-    epsilon = max(epsilon * epsilon_decay, min_epsilon) # Zmniejszanie eksploracji
+    # Exploration : Decay epsilon after each episode
+    epsilon = max(epsilon * epsilon_decay, min_epsilon) 
 
+
+# Qtable filename with number of training episodes, epsilon and alpha
+models_directory = "Model"
+filename = f"q_table_{episode}_episodes_{epsilon}_epsilon_{alpha}_alpha.npy"
+
+# Q-Table : Save Q-table as numpy array pickle
+np.save(f"{models_directory}/{filename}", q_table)
+
+
+# Environment : Close
 env.close()
+
+# Test : Test model on single episode with human rendering
+# --------------------------------------------------------
+print(f"Testing model on single episode with human rendering.")
+
+# Inicjalizacja środowiska CartPole-v1
+env_test = gym.make("CartPole-v1", 
+                    render_mode="human")
+
+# State : Get initial episode state
+state_initial_dict = env_test.reset()
+
+# State : Discretize the state to values according to n_states discretization
+state = discretize_state(state_initial_dict[0], state_bounds, n_states)
+
+# Episode : Process episode until not done
+done = False
+while not done:
+    # Action : Choose action according to epsilon-greedy policy
+    action = choose_action(state, force_optimal=True)
+
+    # Episode : Take action and get next state and reward
+    next_state, reward, done, truncated,  _ = env_test.step(action)
+
+    # State : Discretize the next state to values according to n_states discretization
+    next_state = discretize_state(next_state, state_bounds, n_states)
+
+    # Informations : Print
+    print(f"EnvTest : State {state}, action {action}.")
+    
+    # Time : Sleep
+    sleep(0.1)
+
+    # State : Update state
+    state = next_state
+
+# Environment : Close
+env_test.close()
+
+
 
 
